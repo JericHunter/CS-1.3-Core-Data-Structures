c1d43c2234fbd6f7701fba49f93a530f4a67b579
JericHunter <jeric.hunterr@gmail.com>
2020-02-04 15:43:21 -0800

    commenting and lsr

--- Code/search.py
+++ Code/search.py
@@ -9,7 +9,10 @@ def linear_search(array, item):
 
 
 def linear_search_iterative(array, item):
-    # loop over all array values until item is found
+    # TC
+    # Best: O(1) - First item
+    # Average: O(n) - Have to iterate through every item in the array
+    # Worst: O(n) - Have to iterate through every item in the array
     for index, value in enumerate(array):
         if item == value:
             return index  # found
@@ -17,48 +20,58 @@ def linear_search_iterative(array, item):
 
 
 def linear_search_recursive(array, item, index=0):
-    # TODO: implement linear search recursively here
+    # TC
+    # Best: O(1) - First item
+    # Average: O(n) - Have to loop through every item in the array
+    # Worst: O(n) - Have to loop through every item in the array
+    if index > len(array) - 1:
+        return None
+
+    if item == array[index]:
+        return index
+
+    return linear_search_recursive(array, item, index + 1)
     pass
-    # once implemented, change linear_search to call linear_search_recursive
-    # to verify that your recursive implementation passes all tests
 
 
 def binary_search(array, item):
     """return the index of item in sorted array or None if item is not found"""
-    # implement binary_search_iterative and binary_search_recursive below, then
-    # change this to call your implementation to verify it passes all tests
     # return binary_search_iterative(array, item)
     return binary_search_recursive(array, item)
 
 
 def binary_search_iterative(array, item):
-    # TODO: implement binary search iteratively here
+    # TC
+    # Best: O(1) - Middle of array is the item
+    # Average: O(log(n)) - It will always be log(n) time if not best case
+    # Worst: O(log(n)) - It will always be log(n) time if not best case
+    begin = 0
+    end = len(array)- 1
 
-        begin = 0
-        end = len(array)- 1
+    while begin <=  end:
+        mid = begin + (end-begin) // 2
+        mid_value = array[mid]
 
-        while begin <=  end:
-            mid = begin + (end-begin) // 2
-            mid_value = array[mid]
+        if mid_value == item :
+            return mid
 
-            if mid_value == item :
-                return mid
+        elif item < mid_value:
+            # now check left side of the array
+            end = mid - 1
 
-            elif item < mid_value:
-                # now check left side of the array
-                end = mid - 1
-
-            else:  # item greater than mid value
-                # check right side of the array
-                begin = mid + 1
+        else:  # item greater than mid value
+            # check right side of the array
+            begin = mid + 1
 
         return None
-    # once implemented, change binary_search to call binary_search_iterative
-    # to verify that your iterative implementation passes all tests
 
 
 def binary_search_recursive(array, item, left=None, right=None):
-    # TODO: implement binary search recursively here
+    # TC
+    # Best: O(1) - Middle of array is the item
+    # Average: O(log(n)) - Because we have the working array every time
+    # Worst: O(log(n)) - It will always be log(n) time if not best case
+
     if left == None:
         left = 0
 
