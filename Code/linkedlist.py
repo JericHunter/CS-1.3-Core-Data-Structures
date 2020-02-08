0ffb8bb19d6907c293f85879abf2e27733c990c6
JericHunter <jeric.hunterr@gmail.com>
2020-02-07 21:54:11 -0800

    get index

--- Code/linkedlist.py
+++ Code/linkedlist.py
@@ -78,9 +78,16 @@ class LinkedList(object):
         # Check if the given index is out of range and if so raise an error
         if not (0 <= index < self.size):
             raise ValueError('List index out of range: {}'.format(index))
+        # TODO: Find the node at the given index and return its data
+        node = self.head
+        given_idex = 0
 
+        while node is not None:
+            if index == given_index:
+                return node.data
+            node = node.next
+             given_index +=1
 
-            
     def insert_at_index(self, index, item):
         """Insert the given item at the given index in this linked list, or
         raise ValueError if the given index is out of range of the list size.
@@ -89,7 +96,7 @@ class LinkedList(object):
         # Check if the given index is out of range and if so raise an error
         if not (0 <= index <= self.size):
             raise ValueError('List index out of range: {}'.format(index))
-
+        # TODO: Find the node before the given index and insert item after it
 
     def append(self, item):
         """Insert the given item at the tail of this linked list.
@@ -144,8 +151,9 @@ class LinkedList(object):
         using the same node, or raise ValueError if old_item is not found.
         Best case running time: ??? under what conditions? [TODO]
         Worst case running time: ??? under what conditions? [TODO]"""
-
-
+        # TODO: Find the node containing the given old_item and replace its
+        # data with new_item, without creating a new node object
+        pass
 
     def delete(self, item):
         """Delete the given item from this linked list, or raise ValueError.
