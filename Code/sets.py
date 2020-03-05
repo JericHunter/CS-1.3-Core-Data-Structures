3c12ef9b1ea23d0f6a52774b8c2d52d152aef5ef
JericHunter <jeric.hunterr@gmail.com>
2020-03-05 13:19:19 -0800

    sets

--- Code/sets.py
+++ Code/sets.py
@@ -25,4 +25,50 @@ class HashSet(object):
             return True
 
     def remove(self, element):
+        """Remove the element from this set"""
         self.ht.delete(element)
+
+    def union(self, other_set):
+        new_set = HashSet()
+
+        for item in self.ht.keys():
+            new_set.add(item)
+
+        for item in other_set.ht.keys():
+            new_set.add(item)
+
+        return new_set
+
+    def intersection(self, other_set):
+        """Returns a new set that is the intersection of this set and other_set"""
+        new_set = HashSet()
+
+        for item in self.ht.keys():
+            if other_set.contains(item):
+                new_set.add(item)
+
+        for item in other_set.ht.keys():
+            if self.contains(item):
+                new_set.add(item)
+
+        return new_set
+
+
+
+    def difference(self, other_set):
+        """Returns a new set that is the difference of this set and other_set"""
+        new_set = HashSet()
+
+        for item in other_set.ht.keys():
+            if self.contains(item) == False:
+                new_set.add(item)
+
+        return new_set
+
+    def is_subset(self, other_set):
+        """Returns a boolean indicating whether other_set is a subset of this set"""
+        for item in other_set.ht.keys():
+            if self.contains(item) == False:
+                return False
+
+        return True
