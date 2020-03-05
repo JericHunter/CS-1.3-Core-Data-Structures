52cc04d79f02b249d14ab94bdb6af47a429d76e8
JericHunter <jeric.hunterr@gmail.com>
2020-03-04 23:08:04 -0800

    add n rem

--- Code/sets.py
+++ Code/sets.py
@@ -15,3 +15,14 @@ class HashSet(object):
     def contains(self, element):
         """Returns a boolean to indicate whether an element is in this set"""
         return self.ht.contains(element)
+
+    def add(self, element):
+        """Add an element to this set if it is not already present"""
+        if self.contains(element):
+            return False
+        else:
+            self.ht.set(element,1)
+            return True
+
+    def remove(self, element):
+        self.ht.delete(element)
